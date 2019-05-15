56a8d4ed61d53e39e3c48272f13caa32f8daa370
Carsten Bauer <crstnbr@gmail.com>
2019-05-16 01:12:27 +0200

    update

---
+++ src/Bchain.jl
@@ -0,0 +1,47 @@
+"""
+
+calc_product_chain(B,N) -> (R, svs)
+
+Calculate B^N as B*B*B*B*...*B from right to left.
+
+Returns the result `R` and the singular values (column of `svs`) of all intermediate powers (rows of `svs`).
+"""
+function calc_product_chain(B, N)
+    bs = size(B,1)
+    R = Matrix{eltype(B)}(I, bs, bs)
+    svs = zeros(eltype(B), N,bs)
+    svc = 1
+    for k in 1:N
+        R = B * R
+        svs[svc,:] = log.(svdvals(R))
+        svc += 1
+    end
+    return (R, svs)
+end
+
+
+"""
+Does the same as `calc_product_chain` but stabilizes the
+matrix products by intermediate matrix decompositions.
+"""
+function calc_product_chain_stabilized(B, N, decomposition_method)
+    svs = zeros(eltype(B), N,size(B,1))
+    U, D, X = decomposition_method(B)
+    svs[1,:] = log.(D)
+    svc = 2
+
+    for k in 2:N
+        # multiply B from the left to UDX
+        # and update UDX
+        U = B * U
+        U *= Diagonal(D)
+        U, D, Xnew = decomposition_method(U)
+        X = Xnew * X
+        
+        # keep singular values
+        svs[svc,:] = log.(D)
+        svc += 1
+    end
+    
+    return (U * Diagonal(D) * X, svs)
+end
--- src/StableDQMC.jl
+++ src/StableDQMC.jl
@@ -6,42 +6,43 @@ using GenericSVD, JacobiSVD
 include("linalg.jl")
 include("qr.jl")
 include("svd.jl")
+include("Bchain.jl")
 
-export decompose_udv!,
-    inv_one_plus_udv_alt,
-    inv_sum_udvs!
-    UDT_to_mat,
-    inv_one_plus_udv_scalettar,
-    inv_udt,
-    UDT_to_mat!,
-    inv_one_plus_udv_scalettar!,
-    inv_udt!
-    UDV_to_mat,
-    inv_one_plus_two_udts!,
-    inv_sum_udts,
-    inv_udv,
-    UDV_to_mat!,
-    inv_one_plus_udt,
-    inv_sum_udts!,
-    inv_udv!,
-    decompose_udt,
-    inv_one_plus_udt!,
-    inv_sum_udts_scalettar,
-    multiply_safely,
-    decompose_udt!,
-    inv_one_plus_udt_scalettar!,
-    inv_sum_udts_scalettar!,
-    multiply_safely_udv,
-    decompose_udv,
-    inv_one_plus_udv,
-    inv_sum_udvs,
-    gesdd!,
-    gesdd,
-    gesvd!,
-    gesvd,
-    genericsvd!,
-    genericsvd,
-    gesvj!,
-    gesvj
+export decompose_udv!
+export inv_one_plus_udv_alt
+export inv_sum_udvs!
+export UDT_to_mat
+export inv_one_plus_udv_scalettar
+export inv_udt
+export UDT_to_mat!
+export inv_one_plus_udv_scalettar!
+export inv_udt!
+export UDV_to_mat
+export inv_one_plus_two_udts!
+export inv_sum_udts
+export inv_udv
+export UDV_to_mat!
+export inv_one_plus_udt
+export inv_sum_udts!
+export inv_udv!
+export decompose_udt
+export inv_one_plus_udt!
+export inv_sum_udts_scalettar
+export multiply_safely
+export decompose_udt!
+export inv_one_plus_udt_scalettar!
+export inv_sum_udts_scalettar!
+export multiply_safely_udv
+export decompose_udv
+export inv_one_plus_udv
+export inv_sum_udvs
+export gesdd!
+export gesdd
+export gesvd!
+export gesvd
+export genericsvd!
+export genericsvd
+export gesvj!
+export gesvj
 
 end # module
