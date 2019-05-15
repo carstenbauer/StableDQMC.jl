5bf28282531913cc1cdc062e0f260172e02a2979
Carsten Bauer <crstnbr@gmail.com>
2019-05-15 21:00:32 +0200

    Files generated by PkgTemplates

---
+++ src/StableDQMC.jl
@@ -0,0 +1,5 @@
+module StableDQMC
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using StableDQMC
+using Test
+
+@testset "StableDQMC.jl" begin
+    # Write your own tests here.
+end
