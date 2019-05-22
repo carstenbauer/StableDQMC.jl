40057007067588007ab213a115acf54b7739fd0c
Carsten Bauer <crstnbr@gmail.com>
2019-05-22 13:31:51 +0200

    judging

---
+++ benchmark/judge_two_bench.jl
@@ -0,0 +1,20 @@
+# Takes two .bench files as input
+length(ARGS) >= 2 || error("Please provide two .bench files as input args.")
+
+using Pkg
+Pkg.activate(".")
+
+using PkgBenchmark
+
+f1 = ARGS[1]
+f2 = ARGS[2]
+
+r1 = readresults(f1)
+r2 = readresults(f2)
+
+j = judge(r1, r2)
+
+name1 = replace(f1, ".bench" => "")
+name2 = replace(f2, ".bench" => "")
+
+export_markdown(string("judge_",name1,"_",name2,".md"), j)
