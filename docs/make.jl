974bf0999c8ca6af742631ff8cebbe7a1737c9a3
Carsten Bauer <crstnbr@gmail.com>
2020-02-17 21:14:23 +0200

    basic doc setup

---
+++ docs/make.jl
@@ -0,0 +1,17 @@
+using Documenter, StableDQMC
+
+makedocs(
+    modules = [StableDQMC],
+    doctest = false,
+    sitename = "StableDQMC.jl",
+    pages = [
+        "Introduction" => "index.md",
+    ],
+    # assets = ["assets/custom.css", "assets/custom.js"]
+)
+
+deploydocs(
+    repo = "github.com/crstnbr/StableDQMC.jl.git",
+    push_preview = true,
+    # target = "site",
+)
